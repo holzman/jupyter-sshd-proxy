b0640704fb5455e34dc2df2c0819b4278e09fc21
Burt Holzman <burt@fnal.gov>
2025-03-07 09:41:41 -0600

    Add StrictModes=no, only enable for specified UIDs

--- jupyter_sshd_proxy/__init__.py
+++ jupyter_sshd_proxy/__init__.py
@@ -5,11 +5,24 @@ import subprocess
 
 from typing import Any, Dict
 
+ALLOWED_UID_FILE = "/etc/jsp-allowed-uids"
 HOSTKEY_PATH = os.path.expanduser('~/.ssh/jupyter_sshd_hostkey')
 AUTHORIZED_KEYS_PATH = os.environ.get('JUPYTER_SSHD_PROXY_AUTHORIZED_KEYS_PATH', '.ssh/authorized_keys .ssh/authorized_keys2')
 SSHD_LOG_LEVEL = os.environ.get('JUPYTER_SSHD_PROXY_LOG_LEVEL', 'INFO')
 
+def is_current_user_allowed():
+    try:
+        with open(ALLOWED_UID_FILE, "r") as f:
+            allowed_uids = {int(line.strip()) for line in f if line.strip().isdigit()}
+        return os.getuid() in allowed_uids
+    except Exception as e:
+        print(f"Error reading {ALLOWED_UID_FILE}: {e}")
+        return False
+
 def setup_sshd() -> Dict[str, Any]:
+    if not is_current_user_allowed():
+        return False
+
     if not os.path.exists(HOSTKEY_PATH):
         # Create a per-user hostkey if it does not exist
         os.makedirs(os.path.dirname(HOSTKEY_PATH), mode=0o700, exist_ok=True)
@@ -22,8 +35,10 @@ def setup_sshd() -> Dict[str, Any]:
         # Intentionally have sshd ignore global config
         '-f', 'none',
         '-o', 'ListenAddress 127.0.0.1:{port}',
+        '-o', 'PidFile none',
         # Last login info is from /var/log/lastlog, which is transient in containerized systems
         '-o', 'PrintLastLog no',
+        '-o', 'StrictModes no',
         '-o', f'AuthorizedKeysFile {AUTHORIZED_KEYS_PATH}',
         '-o', f'LogLevel {SSHD_LOG_LEVEL}',
         # Default to enabling sftp
